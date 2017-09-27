2712b2fcb707f57b98c1d8673750e39b395b8480
Chris Johnson <cfjohnson@gmail.com>
2017-09-27 00:05:26 -0400

    warning and time change

--- PortTest.sh
+++ PortTest.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # This script uses netcat to check if a given port is open or shut on a given host
-echo What is the address?
+echo THIS WILL FAIL IF HOST IS OFFLINE. CHECK THAT FIRST.
+echo
+echo What is the host address?
 read HOST
 echo What is the port?
 read PORT
 echo Results:
-nc -w 2 -v $HOST $PORT </dev/null; echo $?
+nc -v $HOST $PORT </dev/null; echo $?
--- update.sh
+++ update.sh
@@ -1,32 +1,32 @@
 #!/bin/zsh
 
-echo -e "\e[7m (01/20) Initiated: brew update 😯\e[0m"
+echo -e "😯 (01/20) Initiated: brew update 😯"
 brew update
-echo -e "\e[7m (02/20) Completed: brew update ✅\e[0m"
-echo -e "\e[7m (03/20) Initiated: brew upgrade 😯\e[0m"
+echo -e "✅ (02/20) Completed: brew update ✅"
+echo -e "✅ (03/20) Initiated: brew upgrade 😯"
 brew upgrade
-echo -e "\e[7m (04/20) Completed: brew upgrade ✅\e[0m"
-echo -e "\e[7m (05/20) Initiated: brew cleanup 😯\e[0m"
+echo -e "✅ (04/20) Completed: brew upgrade ✅"
+echo -e "😯 (05/20) Initiated: brew cleanup 😯"
 brew cleanup
-echo -e "\e[7m (06/20) Completed: brew cleanup ✅\e[0m"
-echo -e "\e[7m (07/20) Initiated: brew cask cleanup 😯\e[0m"
+echo -e "✅ (06/20) Completed: brew cleanup ✅"
+echo -e "😯 (07/20) Initiated: brew cask cleanup 😯"
 brew cask cleanup
-echo -e "\e[7m (08/20) Completed: brew cask cleanup ✅\e[0m"
-echo -e "\e[7m (09/20) Initiated: mas upgrade 😯\e[0m"
+echo -e "✅ (08/20) Completed: brew cask cleanup ✅"
+echo -e "😯 (09/20) Initiated: mas upgrade 😯"
 mas upgrade
-echo -e "\e[7m (10/20) Completed: mas upgrade ✅\e[0m"
-echo -e "\e[7m (11/20) Initiated: npm update 😯\e[0m"
+echo -e "✅ (10/20) Completed: mas upgrade ✅"
+echo -e "😯 (11/20) Initiated: npm update 😯"
 npm i -g npm
-echo -e "\e[7m (12/20) Completed: npm update ✅\e[0m"
-echo -e "\e[7m (13/20) Initiated: gem update -system 😯\e[0m"
+echo -e "✅ (12/20) Completed: npm update ✅"
+echo -e "😯 (13/20) Initiated: gem update -system 😯"
 gem update —system
-echo -e "\e[7m (14/20) Completed: gem update -system ✅\e[0m"
-echo -e "\e[7m (15/20) Initiated: gem update 😯\e[0m"
+echo -e "✅ (14/20) Completed: gem update -system ✅"
+echo -e "😯 (15/20) Initiated: gem update 😯"
 gem update
-echo -e "\e[7m (16/20) Completed: gem update ✅\e[0m"
-echo -e "\e[7m (17/20) Initiated: softareupdate --list 😯\e[0m"
+echo -e "✅ (16/20) Completed: gem update ✅"
+echo -e "😯 (17/20) Initiated: softareupdate --list 😯"
 softwareupdate --list
-echo -e "\e[7m (18/20) Completed: softareupdate --list ✅\e[0m"
-echo -e "\e[7m (19/20) Initiated: softwareupdate --install --all 😯\e[0m"
+echo -e "✅ (18/20) Completed: softareupdate --list ✅"
+echo -e "😯 (19/20) Initiated: softwareupdate --install --all 😯"
 softwareupdate --install --all
-echo -e "\e[7m (20/20) Completed: softwareupdate --install --all ✅\e[0m"
+echo -e "✅ (20/20) Completed: softwareupdate --install --all ✅"
